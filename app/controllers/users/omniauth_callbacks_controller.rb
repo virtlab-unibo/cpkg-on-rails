588c35d80043852752a453fd7ecb756f6c79524c
Pietro Donatini <pietro.donatini@unibo.it>
2013-06-13 11:04:39 +0200

    shibboleth and google authentication

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,30 +1,26 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  # FIXME FIXME FIXME
+  # https://github.com/plataformatec/devise/issues/2432
   skip_before_filter :verify_authenticity_token
+  before_filter :log_omniauth, :check_invitation, :log_if_email
 
-  # email="donapieppo@gmail.com" first_name="Pieppo" last_name="Dona" name="Pieppo Dona"
+  # email="name.surname@gmail.com" first_name="Name" last_name="Surname" name="Name Surname"
   def google
-    logger.info("google uid = #{request.env['omniauth.auth'].uid}")
-    logger.info("google info = #{request.env['omniauth.auth'].info}")
+    #logger.info("using google")
+  end
 
+  # email="usrBase@testtest.unibo.it" last_name="Base" name="SSO"
+  def shibboleth
+    #logger.info("using shibboleth")
+  end
+
+  def log_if_email
     oinfo = request.env['omniauth.auth'].info
-
-    # if invited we create the user and log
-    if params[:uuid] and Invitation.validate_uuid(params[:uuid], oinfo.email)
-      logger.info("creating user on invitation #{params[:uuid]} #{oinfo.inspect}")
-      user = User.create!(:email   => oinfo.email, 
-                          :name    => oinfo.first_name, 
-                          :surname => oinfo.last_name)
-      logger.info("created user #{user} by invitation")
-    end
-
-    user = User.where(:email => oinfo.email).first
-
+    user = User.where(:email => oinfo.email.downcase).first
     if user
       logger.info "logging #{user.inspect} with sign_in_and_redirect"
-      #sign_in_and_redirect user, :event => :authentication    
-      sign_in user
-      redirect_to root_path
+      sign_in_and_redirect user, :event => :authentication    
+      #sign_in user
+      #redirect_to root_path
     else
       flash[:error] = "User #{oinfo.email} is not allowed. Please contact #{Rails.configuration.support_mail}"
       logger.info "#{oinfo.inspect} not allowed"
@@ -32,13 +28,22 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     end
   end
 
-  def shibboleth
+  def log_omniauth
     logger.info("shibboleth uid = #{request.env['omniauth.auth'].uid}")
     logger.info("shibboleth info = #{request.env['omniauth.auth'].info}")
-
-    oinfo = request.env['omniauth.auth'].info
-    raise oinfo.inspect
   end
+  
+  # if invited we create the user and log
+  def check_invitation
+    if params[:uuid] and Invitation.validate_uuid(params[:uuid], oinfo.email.downcase)
+      logger.info("creating user on invitation #{params[:uuid]} #{oinfo.inspect}")
+      user = User.create!(:email   => oinfo.email.downcase, 
+                          :name    => oinfo.first_name || oinfo.name, 
+                          :surname => oinfo.last_name)
+      logger.info("created user #{user} on invitation")
+    end
+  end
+
 
   # we an use fake user only if
   # 1) configured in config/initializers
