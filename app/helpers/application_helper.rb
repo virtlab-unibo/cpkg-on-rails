6930926e0839ab283b7b069809891ed0289dc054
Pietro Donatini <donatini@dm.unibo.it>
2013-10-28 16:13:18 +0100

    logout link. FIXME

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,15 @@ module ApplicationHelper
   def icon(name, options = { :text => "", :size => "18" })
     "<i style=\"font-size: #{options[:size]}px\" class=\"icon icon-#{name.to_s}\"></i> #{options[:text]}".html_safe
   end
+
+  def logout_link
+    dest = case Rails.configuration.omniauth_default 
+           when :shibboleth
+             Rails.env.production? ? 'https://idp.unibo.it/adfs/ls/Prelogout.aspx' : 'https://idptest.unibo.it/adfs/ls/Prelogout.aspx'
+           when :google
+             logout_path
+           end
+    link_to (image_tag(Rails.configuration.login_icon) + '<strong>Logout</strong>'.html_safe), dest
+  end
+
 end
