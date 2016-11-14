e2f768fb2233241f49f8a4e7efb7e59b40112c60
Pietro <donapieppo@yahoo.it>
2016-11-14 09:33:09 +0100

    changes for new dm_unibo_common

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,32 +1,17 @@
 class User < ActiveRecord::Base
   include DmUniboCommon::User
 
-  ##attr_accessible :email, :name, :surname
-  ##attr_accessible :admin, :as => :admin
+  validates :email, presence: {}
+  validates :email, uniqueness: {}
 
-  validates_presence_of :email
-  validates_uniqueness_of :email
-
-  #NOTE: we don't have an id in the courses_users table. 
-  # We can't declare a composite primary key.
-  # Exception raised with has_many -> UnknownPrimaryKey
-  # Solution -> use has_and_belongs_to_many
   has_and_belongs_to_many :courses
- # has_many :courses_users
- # has_many :courses, :through => :courses_users
+  # has_many :courses_users
+  # has_many :courses, :through => :courses_users
   has_many :changelogs
   has_many :packages, :through => :changelogs
 
   def is_admin?
     self.admin
-  end
-
-  def to_s
-    if self.name
-      self.name + " " + self.surname
-    else
-      self.email
-    end
   end
 
   def ==(another_user)
