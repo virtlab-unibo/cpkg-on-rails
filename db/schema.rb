cbc6b4137e752f3c733ab9bae986c5aebfafae59
Pietro <donapieppo@yahoo.it>
2016-11-14 09:33:49 +0100

    rails 5 new schema format

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -13,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 20120828221401) do
 
-  create_table "archives", force: true do |t|
+  create_table "archives", unsigned: true, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string  "uri",          limit: 250
     t.string  "distribution", limit: 50
     t.string  "component",    limit: 50
@@ -21,75 +20,74 @@ ActiveRecord::Schema.define(version: 20120828221401) do
     t.boolean "global"
   end
 
-  create_table "changelogs", force: true do |t|
-    t.integer "package_id",                null: false
-    t.integer "user_id",                   null: false
+  create_table "changelogs", unsigned: true, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+    t.integer "package_id",                  null: false, unsigned: true
+    t.integer "user_id",                     null: false, unsigned: true
     t.string  "version"
-    t.text    "description"
-    t.string  "date"
+    t.text    "description",   limit: 65535
+    t.string  "date",          limit: 100
     t.string  "urgency",       limit: 100
     t.string  "distributions", limit: 100
+    t.index ["package_id"], name: "package_id", using: :btree
+    t.index ["user_id"], name: "user_id", using: :btree
   end
 
-  add_index "changelogs", ["package_id"], name: "package_id", using: :btree
-  add_index "changelogs", ["user_id"], name: "user_id", using: :btree
-
-  create_table "courses", force: true do |t|
+  create_table "courses", unsigned: true, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string  "name"
     t.integer "degree_id"
-    t.text    "description"
+    t.text    "description", limit: 65535
     t.integer "year"
     t.string  "abbr",        limit: 10
   end
 
-  create_table "courses_users", id: false, force: true do |t|
-    t.integer "user_id",   null: false
-    t.integer "course_id", null: false
+  create_table "courses_users", id: false, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "user_id",   null: false, unsigned: true
+    t.integer "course_id", null: false, unsigned: true
+    t.index ["course_id"], name: "course_id", using: :btree
+    t.index ["user_id"], name: "user_id", using: :btree
   end
 
-  add_index "courses_users", ["course_id"], name: "course_id", using: :btree
-
-  create_table "degrees", force: true do |t|
+  create_table "degrees", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string "code", limit: 6, null: false
     t.string "name",           null: false
   end
 
-  create_table "documents", force: true do |t|
-    t.integer  "package_id",                      null: false
+  create_table "documents", unsigned: true, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "package_id",                        null: false, unsigned: true
     t.string   "name",                limit: 200
-    t.text     "description"
+    t.text     "description",         limit: 65535
     t.datetime "created_at"
     t.string   "attach_file_name",    limit: 250
     t.string   "attach_content_type", limit: 100
     t.integer  "attach_file_size"
     t.datetime "attach_updated_at"
-    t.text     "install_path"
+    t.text     "install_path",        limit: 65535
+    t.index ["package_id"], name: "package_id", using: :btree
   end
 
-
-  create_table "invitations", force: true do |t|
+  create_table "invitations", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string "email",                  null: false
     t.string "uuid",       limit: 200, null: false
     t.date   "expiration"
   end
 
-  create_table "packages", force: true do |t|
-    t.integer "archive_id"
-    t.integer "course_id"
-    t.string  "name",              null: false
+  create_table "packages", unsigned: true, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer "archive_id",                                   unsigned: true
+    t.integer "course_id",                                    unsigned: true
+    t.string  "name",                            null: false
     t.string  "short_description"
     t.string  "homepage"
-    t.text    "long_description"
-    t.text    "depends"
+    t.text    "long_description",  limit: 65535
+    t.text    "depends",           limit: 65535
     t.string  "version"
-    t.text    "filename"
+    t.text    "filename",          limit: 65535
+    t.index ["archive_id"], name: "archive_id", using: :btree
+    t.index ["course_id"], name: "course_id", using: :btree
+    t.index ["name"], name: "package_name", using: :btree
   end
 
-  add_index "packages", ["archive_id"], name: "archive_id", using: :btree
-  add_index "packages", ["name"], name: "package_name", using: :btree
-
-  create_table "scripts", force: true do |t|
-    t.integer  "package_id"
+  create_table "scripts", unsigned: true, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+    t.integer  "package_id",                      unsigned: true
     t.string   "name"
     t.string   "stype",               limit: 20
     t.datetime "created_at"
@@ -99,7 +97,7 @@ ActiveRecord::Schema.define(version: 20120828221401) do
     t.integer  "attach_file_size"
   end
 
-  create_table "users", force: true do |t|
+  create_table "users", id: :integer, unsigned: true, force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "upn",        null: false
     t.string   "name"
     t.string   "surname"
@@ -107,8 +105,12 @@ ActiveRecord::Schema.define(version: 20120828221401) do
     t.datetime "updated_at"
     t.string   "email"
     t.string   "uid"
+    t.index ["upn"], name: "index_upn_on_users", using: :btree
   end
 
-  add_index "users", ["upn"], name: "index_upn_on_users", using: :btree
-
+  add_foreign_key "courses_users", "courses", name: "courses_users_ibfk_2"
+  add_foreign_key "courses_users", "users", name: "courses_users_ibfk_1"
+  add_foreign_key "documents", "packages", name: "documents_ibfk_1"
+  add_foreign_key "packages", "archives", name: "packages_ibfk_1"
+  add_foreign_key "packages", "courses", name: "packages_ibfk_2"
 end
