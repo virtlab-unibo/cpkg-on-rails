a87417d862970aa94ef371e64bbc63e8e0e08ebc
Pietro <donapieppo@yahoo.it>
2016-11-16 12:23:58 +0100

    from dm_common

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,9 +14,4 @@ class ApplicationController < ActionController::Base
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
   end
-
-  def user_admin!
-    current_user.admin or raise "NO ADMIN"
-  end
-
 end
