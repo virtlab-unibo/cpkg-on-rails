0e52db8bc9a3e2694e41ea0e69b195308670eb98
Pietro <donapieppo@yahoo.it>
2016-11-23 11:20:04 +0100

    moved from guest view to normal

--- app/controllers/guest/packages_controller.rb
+++ app/controllers/guest/packages_controller.rb
@@ -1,13 +1,4 @@
 class Guest::PackagesController < ApplicationController
-  skip_before_action :redirect_unsigned_user
 
-  # remember in config/routes
-  # match ':id', :controller => "guest::packages", :action => "show"
-  # we reach this also with url like '8080-so-2013'
-  def show
-    @package = params[:id] =~ /^\d+$/ ? Package.find(params[:id]) : Package.find_by_name(params[:id])
-    @course  = @package.course
-    @other_packages = @course.packages - [@package]
-  end
 end
 
--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -1,5 +1,15 @@
 # -*- coding: utf-8 -*-
 class PackagesController < ApplicationController
+  skip_before_action :redirect_unsigned_user, only: :show
+
+  # remember in config/routes
+  # match ':id', :controller => "guest::packages", :action => "show"
+  # we reach this also with url like '8080-so-2013'
+  def show
+    @package = params[:id] =~ /^\d+$/ ? Package.find(params[:id]) : Package.find_by_name(params[:id])
+    @course  = @package.course
+    @other_packages = @course.packages - [@package]
+  end
   def new
     @course = Course.find(params[:course_id])
     authorize! :manage, @course
