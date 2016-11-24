e99f106f3f7dd2163221c3555e0ad5efe4fa75b4
Pietro <donapieppo@yahoo.it>
2016-11-24 17:29:01 +0100

    degrees in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  resources :degrees do
+    resources :courses
+  end
+
   resources :courses  do
     resources :packages do
       resources :changelogs
@@ -33,14 +37,16 @@ Rails.application.routes.draw do
 
   get "register", controller: "invitations", action: :register, as: 'register'
 
-  namespace :guest do
-    resources :courses
-    resources :packages
-  end
+  # namespace :guest do
+  #   resources :courses do
+  #     resources :packages
+  #   end
+  #   resources :packages
+  # end
 
   # FIXME 
   # could be more precise about the match: ex \d+-\w+-\d+ for 8014-so-2013
   get ':id', controller: "packages", action: "show", module: "guest", constraints: { id: /\d+-\w+-\d+/ }
 
-  root to: 'guest/courses#index'
+  root to: 'degrees#index'
 end
