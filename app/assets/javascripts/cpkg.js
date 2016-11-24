5919ec1bb65b8c80bc24e7b3243e478fb1f9c59c
Pietro <donapieppo@yahoo.it>
2016-11-24 13:19:01 +0100

    to js from coffeee

---
+++ app/assets/javascripts/cpkg.js
@@ -0,0 +1,26 @@
+// data = [{'name': 'Joe', }, {'name': 'Henry'}, ...]
+// see http://jsfiddle.net/Fresh/bbzt9hcr/
+datumTokenizer = function(datum) {
+        console.log(datum);
+}
+$(function() {
+  packages = new Bloodhound({
+    datumTokenizer: Bloodhound.tokenizers.whitespace,
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    remote: {
+      url: $('#typeahead').attr('data-searchpath') + "&q=%QUERY",
+      wildcard: '%QUERY',
+      transform: function (response) {
+        return $.map($.parseJSON(response.res), function (package) {
+          return { value: package.name };
+        });
+      }
+    }
+  });
+  $('#typeahead').typeahead(null, {
+    source: packages,
+    display: 'value'
+    //minLength: 3
+  });
+});
+
