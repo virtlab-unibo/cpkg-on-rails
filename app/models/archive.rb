4115674e41deacefecdde364c5e9fd8b4b918d52
Pietro <donapieppo@yahoo.it>
2016-11-24 17:26:26 +0100

    dependency destroy

--- app/models/archive.rb
+++ app/models/archive.rb
@@ -1,16 +1,15 @@
 # archivio da cui si scaricano i packages
 class Archive < ActiveRecord::Base
-  # FIXME
-  # has_many :packages, :dependent => :destroy
+  has_many :packages, dependent: :destroy
   has_many :packages
 
   acts_as_apt_source
 
   def self.default_attributes
-    { :uri          => 'http://mi.mirror.garr.it/mirrors/debian/',
-      :distribution => 'stable', 
-      :component    => 'main', 
-      :arch         => 'binary-i386' }
+    { uri:          'http://mi.mirror.garr.it/mirrors/debian/',
+      distribution: 'stable', 
+      component:    'main', 
+      arch:         'binary-i386' }
   end
 
 end
