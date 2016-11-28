10d7decb44fd63dd24fa5a6a3b73411e5a2f7190
Pietro <donapieppo@yahoo.it>
2016-11-28 09:36:58 +0100

    useful for shibboleth auth in unibo only

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,16 @@ class ApplicationController < ActionController::Base
 
   impersonates :user
 
-  before_action :log_current_user, :redirect_unsigned_user, :set_locale
+  before_action :check_shibboleth_user, :log_current_user, :redirect_unsigned_user, :set_locale
+
+  # if auth is shibboleth and user il logged in apache but not here
+  def check_shibboleth_user
+    if Rails.configuration.dm_unibo_common[:omniauth_provider] == :shibboleth and session[:shibboleth_checked] == nil
+      logger.info("checking shibboleth one time")
+      session[:shibboleth_checked] = true
+      redirect_to auth_shibboleth_callback_path and return 
+    end
+  end
 
   def default_url_options(options={})
     { locale: I18n.locale }
