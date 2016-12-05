fce0b2f210c2a6ab0678bc38c67671b412b1719a
Pietro <donapieppo@yahoo.it>
2016-12-05 15:38:14 +0100

    refactor on show course

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,8 +1,16 @@
 class CoursesController < ApplicationController
+  skip_before_action :redirect_unsigned_user, only: [:index, :show]
   before_action :get_course_and_check_permission, only: [:edit, :update]
 
+  # only teacher (current_user)
+  # students sees degree_path(:id) for list of courses FIXME
   def index
-    @courses = current_user.courses.includes(:packages)
+    @courses = current_user.courses.includes(:packages, :degree)
+  end
+
+  def show
+    @course = Course.includes(packages: :documents).find(params[:id])
+    @packages = @course.packages.all
   end
 
   def edit
@@ -15,10 +23,6 @@ class CoursesController < ApplicationController
     else
       render action: :edit
     end
-  end
-
-  def show
-    @course = Course.find(params[:id])
   end
 
   private
