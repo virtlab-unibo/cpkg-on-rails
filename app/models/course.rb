e839e0b07f08814c2cf89adc3f7adaee12b8f71f
Pietro <donapieppo@yahoo.it>
2016-12-07 12:55:20 +0100

    verify_no_dependencies for delete

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,7 +1,5 @@
 class Course < ActiveRecord::Base
   has_and_belongs_to_many :users
-  #has_many :courses_users
-  #has_many :users, :through => :courses_users
   has_many :packages
   belongs_to :degree
 
@@ -9,8 +7,17 @@ class Course < ActiveRecord::Base
   validates_uniqueness_of :name
   validates_format_of :abbr, :with => /\A[a-z0-9][a-z0-9+.-]+\z/, :message => :course_name_format
 
+  before_destroy :verify_no_dependencies
+
   def to_s
     self.degree.code + "-" + self.name
+  end
+ 
+  def verify_no_dependencies
+    if self.packages.any?
+      self.errors.add(:base, 'There are packages')
+      return false
+    end
   end
 
 end
