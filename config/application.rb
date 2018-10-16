d27233cfbd9ec29c06247eb69b68959837ca9c52
Pietro <donapieppo@yahoo.it>
2018-10-16 16:15:35 +0200

    fix. forgotten

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,13 @@ module CpkgOnRails
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.time_zone = 'Rome'
+    config.i18n.default_locale = :it
+
+    config.autoload_paths += %W(#{Rails.root}/app/pdfs)
+
+    config.action_mailer.default_url_options = { protocol: 'https' }
+    config.dm_unibo_common = ActiveSupport::HashWithIndifferentAccess.new config_for(:dm_unibo_common)
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
