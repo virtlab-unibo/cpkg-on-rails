19dac8fecf5dfcb84db2729baac02ef719861a9a
Pietro <donapieppo@yahoo.it>
2018-10-25 15:02:17 +0200

    repreporo

--- app/controllers/changelogs_controller.rb
+++ app/controllers/changelogs_controller.rb
@@ -5,41 +5,37 @@ class ChangelogsController < ApplicationController
     @changelog = @package.changelogs.new
   end
 
-  # create the debian package and put it into the repo_dir
+  # create the debian package and put it into the config.reprepro_incomingdir
   def create
-    # check if all the corepackages are into the dependency
-    # list, if not we add them
-    @package.add_global_deps
     @changelog = @package.changelogs.new(changelog_params)
     @changelog.user_id = current_user.id
     @changelog.date = ActiveDebianRepository::Changelog.date_line
-    # FIXME: make it a user or sysadmin choice
     @changelog.urgency = "medium"
     @changelog.distributions = Rails.configuration.linux_distro
+
+    if ! File.file?(Rails.configuration.reprepro_command)
+      flash[:error] = "Missing #{Rails.configuration.reprepro_command}. Try apt-get install reprepro."
+      render action: :new
+      return
+    end
 
     res = false
     begin
       Package.transaction do
         @changelog.save! 
-        # NB. we've to reload the package contents from 
-        # db to get the right version of the package.
-        # Maybe there's a better way
+        # NB. we've to reload the package contents from db to get the right version of the package.
         @package.reload
-        dest_dir = Rails.configuration.repo_dir
-        equivs = ActiveDebianRepository::Equivs.new(@package, dest_dir) 
-        res = equivs.create!
+        equivs = ActiveDebianRepository::Equivs.new(@package, Rails.configuration.reprepro_incomingdir)
+        equivs.create!
+        flash[:notice] = I18n.t 'pkg_upld_ok'
       end
     rescue => err
       logger.info "ChangelogsController: Failed to create the package: #{err}"
-      # reload the previous state from db. (maybe it's unnecessary)
-      # NB changelog doesn't exist then it doesn't need to be reloaded.
-      @package.reload
-    end
-    if res 
-      flash[:notice] = I18n.t 'pkg_upld_ok' 
-    else
       flash[:error] = I18n.t 'pkg_upld_no'
     end
+
+    `#{Rails.configuration.reprepro_exec}`
+
     redirect_to courses_path
   end
 
