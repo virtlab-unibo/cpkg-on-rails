3b079f209a726466e303a1cb1014869f370f88b0
Pietro <donapieppo@yahoo.it>
2018-11-21 13:51:38 +0100

    first_admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,14 @@ class User < ActiveRecord::Base
   # has_many :courses_users
   # has_many :courses, :through => :courses_users
   has_many :changelogs
-  has_many :packages, :through => :changelogs
+  has_many :packages, through: :changelogs
 
   def ==(another_user)
     self.id == another_user.id
+  end
+
+  def self.first_admin
+    User.where(upn: ADMINS.first).first
   end
 end
 
