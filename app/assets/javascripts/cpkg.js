5127711a1cfd97b245c119507279e8b6e51d0663
Pietro <donapieppo@yahoo.it>
2018-11-22 10:58:00 +0100

    how was working before???

--- app/assets/javascripts/cpkg.js
+++ app/assets/javascripts/cpkg.js
@@ -8,7 +8,7 @@ $(function() {
     datumTokenizer: Bloodhound.tokenizers.whitespace,
     queryTokenizer: Bloodhound.tokenizers.whitespace,
     remote: {
-      url: $('#typeahead').attr('data-searchpath') + "&q=%QUERY",
+      url: $('#typeahead').attr('data-searchpath') + "?q=%QUERY",
       wildcard: '%QUERY',
       transform: function (response) {
         return $.map($.parseJSON(response.res), function (package) {
