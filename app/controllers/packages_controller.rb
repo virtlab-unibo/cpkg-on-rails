ebe47882c49aaa5499682adf4708a88fea06ad5c
Pietro <donapieppo@yahoo.it>
2018-11-22 10:58:48 +0100

    show is in vlabpackages

--- app/controllers/packages_controller.rb
+++ app/controllers/packages_controller.rb
@@ -2,10 +2,10 @@ class PackagesController < ApplicationController
 
   # reach this also with url like '8080-so-2013'
   # get ':id', controller: "packages", action: "show", constraints: { id: /\d+-\w+-\d+/ }
-  def show
-    @package = params[:id] =~ /^\d+$/ ? Package.find(params[:id]) : Package.find_by_name(params[:id])
-    @other_packages = @package.course.vlab_packages - [@package]
-  end
+  #def show
+  #  @package = params[:id] =~ /^\d+$/ ? Package.find(params[:id]) : Package.find_by_name(params[:id])
+  #  @other_packages = @package.course.vlab_packages - [@package]
+  #end
 
   def search
     logger.info(params.inspect)
